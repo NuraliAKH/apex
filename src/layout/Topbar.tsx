67f2c8c50457832c781cc2df3a43047c28a1eb95
NuraliAKH <axmadovnurali5@gmail.com>
2025-07-28 20:56:34 +0500

    fixed topbar

--- src/layout/Topbar.tsx
+++ src/layout/Topbar.tsx
@@ -8,20 +8,43 @@ const Topbar = () => {
       </div>
 
       <div className={styles.actions}>
-        <button>
+        <button className={styles.actionButton}>
           <img src="/topbar/first.svg" alt="" />
+          Оформить E-polis
         </button>
         <button className={styles.actionButton}>
           <img src="/topbar/sec.svg" alt="show" />
           Такафул
         </button>
+
         <button className={styles.actionButton}>
-          <img src="/topbar/th.svg" alt="" /> | RU
+          <span
+            style={{
+              display: "flex",
+              alignItems: "center",
+              borderRight: "2px solid #999",
+              paddingRight: 8,
+              marginRight: 8,
+            }}
+          >
+            <img src="/topbar/th.svg" alt="Flag" />
+          </span>
+          <span>RU</span>
         </button>
+
         <button className={styles.actionButton}>
-          <img src="/topbar/four.svg" alt="1188" />
-          1188 |
-          <img src="/topbar/five.svg" alt="Поддержка" />
+          <span
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "5px 8px 5px",
+              borderRight: "2px solid #999",
+            }}
+          >
+            <img src="/topbar/four.svg" alt="1188" />
+            <span style={{ marginLeft: 4 }}>1188</span>
+          </span>
+          <img src="/topbar/five.svg" alt="Поддержка" style={{ marginRight: 4 }} />
           Поддержка
         </button>
       </div>
