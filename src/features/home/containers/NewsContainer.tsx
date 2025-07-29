9c919db4b921c8c769b9e940fdd51c2fd9009a9b
NuraliAKH <axmadovnurali5@gmail.com>
2025-07-30 02:57:00 +0500

    news mobile

--- src/features/home/containers/NewsContainer.tsx
+++ src/features/home/containers/NewsContainer.tsx
@@ -1,13 +1,18 @@
 import "./news.css";
 import { newsData } from "../data/News";
 import { NewsCard } from "../components/NewsCard";
+import { useMediaQuery } from "react-responsive";
 
 export const NewsContainer = () => {
+  const isMobile = useMediaQuery({ maxWidth: 480 });
+
+  const dataToRender = isMobile ? newsData.slice(0, 1) : newsData;
+
   return (
     <div style={{ marginTop: "40px", gap: "15px", display: "flex", flexDirection: "column" }}>
       <h3>Новости</h3>
       <div className="news-container">
-        {newsData.map((item: any) => (
+        {dataToRender.map((item: any) => (
           <NewsCard key={item.id} item={item} />
         ))}
       </div>
